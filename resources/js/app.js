dfb50319cd1549bc6ecead7986d0cc24bef21298
Nguyen <phannguyenhoainguyen@gmail.com>
2022-12-13 10:01:48 +0700

    setup vue

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,7 @@
-import './bootstrap';
+import "./bootstrap";
+
+import { createApp } from "vue";
+
+import App from "./layouts/app.vue";
+
+createApp(App).mount("#app");
---
+++ resources/js/layouts/app.vue
@@ -0,0 +1,4 @@
+<template>
+    Cinerama Cinema
+</template>
+
